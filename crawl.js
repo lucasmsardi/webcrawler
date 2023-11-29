1749222e83691f37237140300ba0fd4f40556140
lucasmsardi <lucasmsardi@gmail.com>
2023-11-29 15:55:23 -0300

    crawl page com error handling

--- crawl.js
+++ crawl.js
@@ -1,3 +1,54 @@
+const { JSDOM } = require('jsdom')
+
+async function crawlPage(currentURL) {
+    console.log(`actively crawling: ${currentURL}`)
+
+    try {
+        const resp = await fetch(currentURL)
+        if (resp.status > 399) {
+            console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`)
+            return
+        }
+
+        const contentType = resp.headers.get("content-type")
+        if (!contentType.includes("text/html")) {
+            console.log(`non html response, content type: ${contentType} on page: ${currentURL}`)
+            return
+        }
+
+        console.log(await resp.text())
+    } catch (err) {
+        console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
+    }
+
+}
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) === '/') {
+            // relative URLs
+            try { 
+                const urlObj = new URL(`{baseURL}${linkElement.href}`)
+                urls.push(urlOb.href)
+            } catch (err) {
+                console.log(`error with relative url: ${err.message}`)
+            }
+        } else {
+            //absolute URLs
+            try { 
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlOb.href)
+            } catch (err) {
+                console.log(`error with absolute url: ${err.message}`)
+            }
+        }
+    }
+    return urls
+}
+
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
     const hostPath =  `${urlObj.hostname}${urlObj.pathname}`
@@ -8,5 +59,7 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML, 
+    crawlPage
 }
