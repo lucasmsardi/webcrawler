15d425668b9b4f6f866faf0da1a4903923539d0b
lucasmsardi <lucasmsardi@gmail.com>
2023-11-29 16:28:57 -0300

    printReport funcionando

--- crawl.js
+++ crawl.js
@@ -1,26 +1,47 @@
 const { JSDOM } = require('jsdom')
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+    const baseURLobj = new URL(baseURL) 
+    const currentURLobj = new URL(currentURL) 
+    if (baseURLobj.hostname !== currentURLobj.hostname) {
+        return pages
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL)
+    if (pages[normalizedCurrentURL] > 0) {
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+
+    pages[normalizedCurrentURL] = 1
+
     console.log(`actively crawling: ${currentURL}`)
 
     try {
         const resp = await fetch(currentURL)
         if (resp.status > 399) {
             console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`)
-            return
+            return pages
         }
 
         const contentType = resp.headers.get("content-type")
         if (!contentType.includes("text/html")) {
             console.log(`non html response, content type: ${contentType} on page: ${currentURL}`)
-            return
+            return pages
         }
 
-        console.log(await resp.text())
+        const htmlBody = await resp.text()
+
+        nextURLs = getURLsFromHTML(htmlBody, baseURL)
+
+        for(const nextURL of nextURLs) {
+            pages = await crawlPage(baseURL, nextURL, pages)
+        }
+
     } catch (err) {
         console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
     }
-
+    return pages
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
@@ -32,7 +53,7 @@ function getURLsFromHTML(htmlBody, baseURL) {
             // relative URLs
             try { 
                 const urlObj = new URL(`{baseURL}${linkElement.href}`)
-                urls.push(urlOb.href)
+                urls.push(urlObj.href)
             } catch (err) {
                 console.log(`error with relative url: ${err.message}`)
             }
@@ -40,7 +61,7 @@ function getURLsFromHTML(htmlBody, baseURL) {
             //absolute URLs
             try { 
                 const urlObj = new URL(linkElement.href)
-                urls.push(urlOb.href)
+                urls.push(urlObj.href)
             } catch (err) {
                 console.log(`error with absolute url: ${err.message}`)
             }
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { crawlPage } = require('./crawl.js')
+const { printReport }  = require('./report.js')
 
-function main() {
+async function main() {
     if (process.argv.length < 3) {
         console.log("no website provided")
         process.exit(1)
@@ -15,7 +16,9 @@ function main() {
     const baseURL = process.argv[2]
 
     console.log(`starting crawl of ${baseURL}`)
-    crawlPage(baseURL)
+    const pages = await crawlPage(baseURL, baseURL, {})
+
+    printReport(pages)
 }
 
 main()
